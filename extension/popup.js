4b26f65bad3bcb82c1abca54de4c7d7c66935dab
BigBlueHat <byoung@bigbluehat.com>
2017-03-29 08:40:15 -0400

    Remove jQuery dependency; v0.0.2

--- extension/popup.js
+++ extension/popup.js
@@ -1,16 +1,22 @@
-let $links = $('#links');
+links = document.getElementById('links');
 
 chrome.tabs.executeScript(null, {
   file: 'extractor.js'
 }, (results) => {
   let anchors = results[0];
   anchors.forEach((a) => {
-    let tr = `<tr>
-      <td><a href="${a.href}" target="_blank">${a.href}</a><br />
-      ${a.text}
-      </td>
-      <td>${a.rel}</td>
-    </tr>`;
-    $links.append(tr);
+    let tr = document.createElement('tr');
+      let td_url = document.createElement('td');
+        let link = document.createElement('a');
+        link.href = a.href;
+        link.target = '_blank';
+        link.textContent = a.href;
+        let br = document.createElement('br');
+      td_url.append(link, br, a.text);
+
+      let td_rel = document.createElement('td');
+      td_rel.textContent = a.rel;
+    tr.append(td_url, td_rel);
+    links.append(tr);
   });
 });
